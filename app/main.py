1126ece056551d5437405dec3aa6037dee0f8f18
matista19 <matista19@gmail.com>
2024-06-20 15:12:24 +0200

    implementing Langchain with Langserve for extra routes

--- app/main.py
+++ app/main.py
@@ -4,33 +4,42 @@ from fastapi import FastAPI
 from .database import init_db
 from app import routers
 from fastapi.openapi.utils import get_openapi
-
-# import uvicorn
-# import openai
-# from langchain.prompts import ChatPromptTemplate
-# from langchain.chat_models import ChatOpenAI
-# from langchain_core.messages import HumanMessage
-# from langchain_mistralai.chat_models import ChatMistralAI
+from langserve import add_routes
+from langchain_core.messages import HumanMessage
+from langchain_mistralai.chat_models import ChatMistralAI
+from langchain.prompts import ChatPromptTemplate
 
 load_dotenv()
-app = FastAPI(swagger_ui_parameters={"operationsSorter": "method"}) 
 
+app = FastAPI(
+    title="FastAPI with JWT, Postgres and LangChain Server",
+    version="1.0",
+    description="A simple API server using LangChain's Runnable interfaces",
+    swagger_ui_parameters={"operationsSorter": "method"}
+)
 
 app.include_router(routers.authentication.router)
 app.include_router(routers.prompt.router)
 app.include_router(routers.user.router)
 
+model = ChatMistralAI()
+prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
+
+add_routes(
+    app,
+    model,
+    path="/chat",
+)
+
+#add_routes(
+#    app,
+#    prompt | model,
+#    path="/generate",
+#)
 
 @app.on_event("startup")
 async def on_startup():
     await init_db()
-
-#openai_api_key = "sk-proj-nYFaQIe0tbyzETLB25vUT3BlbkFJh6eOlaS6s9jUO5UIywez"
-#prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
-#model = ChatOpenAI(model="gpt-3.5-turbo")
-#chain = prompt | model
-#response = chain.invoke({"topic":"chicken"})
-#print(response)
 
 @app.get("/")
 async def read_root():
