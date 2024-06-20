31315279ff232cab4af4f21bda212c180e096600
matista19 <matista19@gmail.com>
2024-06-20 17:01:14 +0200

    add first route first langchain and mistral ai for testing

--- app/main.py
+++ app/main.py
@@ -9,10 +9,6 @@ from langchain_core.messages import HumanMessage
 from langchain_mistralai.chat_models import ChatMistralAI
 from langchain.prompts import ChatPromptTemplate
 
-#from gpt4all import GPT4All
-#from langchain.llms import GPT4AllModel
-#from langchain.prompts import ChatPromptTemplate
-
 load_dotenv()
 
 app = FastAPI(
@@ -26,24 +22,14 @@ app.include_router(routers.authentication.router)
 app.include_router(routers.prompt.router)
 app.include_router(routers.user.router)
 
-model = ChatMistralAI()
-prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
-
-# Initialiser le modèle GPT4All avec Mistral
-#gpt4all = GPT4All(model_name="mistral-7b-openorca.Q4_0.gguf")
-#model = GPT4AllModel(gpt4all=gpt4all)
-#prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
-
 @app.on_event("startup")
 async def on_startup():
     await init_db()
 
-@app.get("/")
-async def read_root():
-    openai_api_key = os.environ.get("OPENAI_API_KEY")
-    print(openai_api_key)
-    # chat = ChatMistralAI()
-    # messages = [HumanMessage(content="Quel artiste chante Bendo na bendo et ou se trouve le bendo ?")]
-    # response = chat.invoke(messages)
-    # print(response)
-    return {"Hello": "World"}
+@app.get("/mistral/{message}", tags=["MistralAI"])
+async def get_mistral_ai(message: str):
+    model = ChatMistralAI()
+    #prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
+    messages = [HumanMessage(content=message)]
+    response = model.invoke(messages)
+    return response
